036a5cf9507064b7b37e2d449cbd56fd7acd301f
bakman <bakman.bakman@gmail.com>
2017-12-08 12:39:35 +0100

    formatted and added graph

--- index.php
+++ index.php
@@ -60,9 +60,10 @@
 	
 	}
 
-	function graph_data($symbol,$currency){
+	function graph_data($symbol){
 		//curl "https://min-api.cryptocompare.com/data/histoday?fsym=$symbol&tsym=$currency&limit=15"
-		$graph = file_get_contents("https://min-api.cryptocompare.com/data/histoday?fsym=$symbol&tsym=$currency&limit=30");
+		//https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=ETH&limit=30&aggregate=1&e=CCCAGG
+		$graph = file_get_contents("https://min-api.cryptocompare.com/data/histohour?fsym=$symbol&tsym=EUR&limit=24");
 		//$graph = file_get_contents("graph.json");
 
 		$graphdata = json_decode($graph,true);
@@ -73,7 +74,6 @@
 		return substr($out,0,strlen($out)-1);
 	}
 
-
 ?>
 <html>
 	<head>
@@ -81,122 +81,72 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="lib/css/styles.css">
 		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-		<script type="text/javascript">
-		$(function(){
-
-			$("#addcoin").click(function(){
-				$("#coinform").toggle()
-			})
-
-	    	$("#cancel").click(function(){
-				$("#coinform").toggle()
-	    	})
-
-	    	var timeout = setInterval(portfolio, 20000);    
-			portfolio()
-			
-		})
-
-		
-		function portfolio(){
-
-			$('#holder').load('index.php #portfolio',function(){
-
-			var sum = 0;
-    	
-	    	$(".paid").each(function() {
-	        	sum += parseFloat($(this).text().replace("€", ""));
-	    	});
-	    	
-	    	$("#total").text("Investment: € "+sum.toFixed(2))
-
-	    	var profits = 0;
-	    	$(".updown").each(function() {
-	        	profits += parseFloat($(this).text().replace("€", ""));
-	    	});
-
-	    	$("#profit").html("Profit/Loss: <span class='profit'>€ "+parseFloat(profits).toFixed(2)+"</span>")
-	    	$("#currentvalue").text("Current value: € "+parseFloat(sum+profits).toFixed(2))
-	    	
-	    	if(profits>0){
-
-	    		$(".profit").removeClass('negative').addClass("positive")
-	    		}else{
-	    		$(".profit").removeClass('positive').addClass("negative")
-    		
-	    	}
-
-	    	$(".updown,.change").addClass('positive')
-	    	$(".updown:contains('-'),.change:contains('-')").removeClass('positive').addClass('negative')
-	    	})
-		}
-
-	</script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-sparklines/2.1.2/jquery.sparkline.min.js"></script>
+		<script src="lib/js/script.js"></script>
 	</head>
 <body>
 	<div class='addcoin'><a href='#' id='addcoin'>+</a></div>
 	<h1>CRYPTOFOLIO</h1>
-	<h2><span id="total"></span><br/>
-		<span id="profit"></span></br>
-		<span id="currentvalue"></span></h2>
+	<h2><span id="total"></span><br/><span id="profit"></span><br/><span id="currentvalue"></span></h2>
 
 <div class="frame">
 	<div id="coinform">
-<form action="index.php" method="get">
-	<h1>Add Coin</h1>
-	<table>
-		<tr>
-			<td>Coin:</td>
-			<td><input type='text' value='' name='symbol' placeholder='Enter Symbol'/></td>
-		</tr>
-		<tr>
-			<td>Total:</td>
-			<td><input type='text' value='' name='total' placeholder='Number of coins' /></td>
-		</tr>
-		<tr>
-			<td>Paid:</td>
-			<td><input type='text' value='' name='amount' placeholder='xxx.xx' /></td>
-		</tr>
-		<tr>
-			<td>Currency:</td>
-			<td><input type='text' value='' name='currency' placeholder='EUR' /></td>
-		</tr>
-		
-		<tr>
-			<td>&nbsp;</td>
-			<td><input type='submit' value='Add Coin' /><input type='button' value='Cancel' id="cancel" /></td>
-		</tr>
-	</table>
-	<input type="hidden" name='action' value='addcoin'>
-</form>
-</div>
-<div id="holder">
-<table id="portfolio">
-	<tr>
-		<th>Coin</th>
-		<th>P/L</th>
-		<th>Current price</td> 
-	</tr>
-<?php
-	foreach ($coins['coins'] as $symbol => $value){
+		<form action="index.php" method="get">
+			<h1>Add Coin</h1>
+			<table>
+				<tr>
+					<td>Coin:</td>
+					<td><input type='text' value='' name='symbol' placeholder='Enter Symbol'/></td>
+				</tr>
+				<tr>
+					<td>Total:</td>
+					<td><input type='text' value='' name='total' placeholder='Number of coins' /></td>
+				</tr>
+				<tr>
+					<td>Paid:</td>
+					<td><input type='text' value='' name='amount' placeholder='xxx.xx' /></td>
+				</tr>
+				<tr>
+					<td>Currency:</td>
+					<td><input type='text' value='' name='currency' placeholder='EUR' /></td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td><input type='submit' value='Add Coin' /><input type='button' value='Cancel' id="cancel" /></td>
+				</tr>
+			</table>
+			<input type="hidden" name='action' value='addcoin'>
+		</form>
+	</div>
+	<div id="holder">
+		<table id="portfolio">
+			<tr>
+				<th>Coin</th>
+				<th>P/L</th>
+				<th>Price</th> 
+				<th>Last 24hrs</th> 
+			</tr>
+		<?php
+			foreach ($coins['coins'] as $symbol => $value){
 
-		$currency = strtoupper($value['currency']);
-		$price = round($priceArray['RAW'][$symbol][$currency]['PRICE'],2);
-		$change = round($priceArray['RAW'][$symbol][$currency]['CHANGEPCT24HOUR'],2);
+				$currency = strtoupper($value['currency']);
+				$price = round($priceArray['RAW'][$symbol][$currency]['PRICE'],2);
+				$change = round($priceArray['RAW'][$symbol][$currency]['CHANGEPCT24HOUR'],2);
 
-		$difference = round($price*$value['owned'],2);
-		$profit = round($difference-$value['paid'],2);
-		
-		echo "<tr>";
-		echo "<td class='symbol'><a href='index.php?action=remove&symbol=".$symbol."' onclick=\"return confirm('Remove ".$symbol." \\nAre you sure?')\">".symbol_lookup($symbol) . "</td>"; 
-		echo "<td><span class='updown'>". currencyformat($currency).round($profit,2)."</span><br/><span class='owned'>".$value['owned']."</span><br/><span class='paid'>".  currencyformat($currency).$value['paid'] . "</span></td>";
-		echo "<td>".currencyformat($currency)." ".$price."<br/><span class='change'>". round($change,2)."%</span></td>";
-		echo "</tr>";
-	}
+				$difference = round($price*$value['owned'],2);
+				$profit = round($difference-$value['paid'],2);
+				
+				echo "<tr>";
+				echo "<td class='symbol'><a href='index.php?action=remove&symbol=".$symbol."' onclick=\"return confirm('Remove ".$symbol." \\nAre you sure?')\">".symbol_lookup($symbol) . "</td>"; 
+				echo "<td class='pl'><span class='updown'>". currencyformat($currency).number_format($profit,2)."</span><br/><span class='owned'>".number_format($value['owned'],8)."</span><br/><span class='paid'>".  currencyformat($currency).number_format($value['paid'],2) . "</span></td>";
+				echo "<td class='currentprice'>".currencyformat($currency)." ".number_format($price,2)."<br/><span class='change'>". number_format($change,2)."%</span></td>";
+				echo "<td class='graphtd'><span class='graph'>". graph_data($symbol)."</span></td>";
+				echo "</tr>";
+			}
 
-?>
-</table>
-</div>
+		?>
+		</table>
+	</div>
 </div>
 </body>
 </html>
