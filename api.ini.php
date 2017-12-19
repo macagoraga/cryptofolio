a2f30d55be207d0bab4a57d267b7ce3a63c0ff8c
bakman <bakman.bakman@gmail.com>
2017-12-19 14:23:11 +0100

    cleanup

--- api.ini.php
+++ api.ini.php
@@ -1,21 +1,14 @@
 ;<?php
 ;die(); // For further security
 ;/*
-<<<<<<< HEAD
+
 investment=""
+
 [keys]
 bittrexapi=""
 bittrexsecret=""
-=======
-
-investment="2950.00"
-
-[keys]
-bittrexapi="519b15ab8729410db8247e885113b63a"
-bittrexsecret="578d53b01d294ab4adfd0a144a6c227b"
 krakenapi=""
 krakensecret=""
->>>>>>> develop
 ;*/
 
 ;?>
