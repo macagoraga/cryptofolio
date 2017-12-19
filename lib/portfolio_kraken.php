387a7cd6876320aaa7efa258304a138b32fade95
bakman <bakman.bakman@gmail.com>
2017-12-19 20:54:05 +0100

    kraken bugs

--- lib/portfolio_kraken.php
+++ lib/portfolio_kraken.php
@@ -1,9 +1,7 @@
 <?php
 
-
 // debug
 //$krakendata = Array('error' => Array(), 'result' => Array('ETH' => 3415.8014, 'ZEUR' => 155.5649, 'XBTC' => 149.9688412800, 'XXRP' => 499889.51600000));
- 
 
 	$apikey=$ini_array['krakenapi'];
 	$apisecret=$ini_array['krakensecret'];
@@ -21,22 +19,24 @@
 		$krakendata = $kraken->QueryPrivate('Balance');
 		
 
-
 		foreach ($krakendata['result'] as $value => $v) {
 
-			
-				$coindata['coins']['kraken'][$value]['wallettype'] = 'exchange';
-				$coindata['coins']['kraken'][$value]['walletname'] = 'kraken';
-		    	$coindata['coins']['kraken'][$value]['name'] = $coinList['Data'][$value]['CoinName'];
-		    	$coindata['coins']['kraken'][$value]['fullname'] = $coinList['Data'][$value]['FullName'];
-		    	$coindata['coins']['kraken'][$value]['owned'] = $v;
+				$symbol = substr($value,-3);
+				
+				if($symbol!='EUR'){
+	
+					if($symbol=='XBT'){
+						$symbol=='BTC';
+					}
+
+					$coindata['coins']['kraken'][$symbol]['wallettype'] = 'exchange';
+					$coindata['coins']['kraken'][$symbol]['walletname'] = 'kraken';
+			    	$coindata['coins']['kraken'][$symbol]['name'] = $coinList['Data'][$symbol]['CoinName'];
+			    	$coindata['coins']['kraken'][$symbol]['fullname'] = $coinList['Data'][$symbol]['FullName'];
+			    	$coindata['coins']['kraken'][$symbol]['owned'] = $v;
 		
+				}
 		}
-
 	}
-
-
-
-
 
 ?>
