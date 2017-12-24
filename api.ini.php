81b9f321495165e434f783c12df62c0e77a21c3d
bakman <bakman.bakman@gmail.com>
2017-12-24 12:21:06 +0100

    api.ini

--- api.ini.php
+++ api.ini.php
@@ -2,12 +2,9 @@
 ;die(); // For further security
 ;/*
 
+
 [portfolio]
-<<<<<<< HEAD
 investment=""
-=======
-investment="4950.00"
->>>>>>> develop
 
 [bittrex]
 api=""
@@ -18,8 +15,8 @@ api=""
 secret=""
 
 [poloniex]
-api="84V9D7A8-P8ICLUJ0-LU1Z125Y-4MINL5VK"
-secret="0627a59cc9b50cfacb8eeccdb3689d3dacd5e2e948fd3013e103c7cca0dc7d3dd7169eac7f018ff7d4bbff830664b95ed587f674d41189058ee7e258f60904d7"
+api=""
+secret=""
 
 
 
