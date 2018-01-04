f5cb44c91ce20a26695e262bad434c94b0f075bf
bakman <bakman.bakman@gmail.com>
2018-01-04 16:40:10 +0100

    overhaul. New UI, techanjs charts

--- graph.php
+++ graph.php
@@ -1,35 +1,53 @@
-<?php
+<?php 
+header('Content-type: application/csv');
+header('Content-Disposition: attachment; filename=graph.csv');
+header("Content-Transfer-Encoding: UTF-8");
+$f = fopen('php://output', 'a');
 date_default_timezone_set('Europe/London');
-	$limit = $_GET['limit'];
-	$symbol = $_GET['symbol'];
+$limit = $_GET['limit'];
+$symbol = $_GET['symbol'];
+if($_GET['type']=='minutes'){
+	
+	$graph = file_get_contents("https://min-api.cryptocompare.com/data/histominute?fsym=$symbol&tsym=EUR&limit=$limit&aggregate=10");
+	$graphdata = json_decode($graph, true);
 
-	if($_GET['type']=='today'){
-		
-    	$graph = file_get_contents("https://min-api.cryptocompare.com/data/histominute?fsym=$symbol&tsym=EUR&limit=$limit");
-    	$graphdata = json_decode($graph, true);
+}
+if($_GET['type']=='hours'){
     
-    }
-    if($_GET['type']=='hour'){
-        
-        $graph = file_get_contents("https://min-api.cryptocompare.com/data/histohour?fsym=$symbol&tsym=EUR&limit=$limit");
-        $graphdata = json_decode($graph, true);
-    
-    }
-    if($_GET['type']=='history'){
-    
-    	$graph = file_get_contents("https://min-api.cryptocompare.com/data/histoday?fsym=$symbol&tsym=EUR&limit=$limit");
-    	$graphdata = json_decode($graph, true);
-    
-    }
-    
-    $grapharray = array();
-    foreach($graphdata['Data'] as $key => $value)
-    {
-    	
-    	$ts = date("Y-m-d H:i:s",$value['time']);
+    $graph = file_get_contents("https://min-api.cryptocompare.com/data/histohour?fsym=$symbol&tsym=EUR&limit=$limit");
+    $graphdata = json_decode($graph, true);
 
-       $grapharray[] = array("TIMESTAMP"=>$ts, "OPEN"=>$value['open'], "CLOSE"=>$value['close'], "HIGH"=>$value['high'],"LOW"=>$value['low'], "VOLUME"=>$value['volumeto']);
+}
+if($_GET['type']=='days'){
+
+	$graph = file_get_contents("https://min-api.cryptocompare.com/data/histoday?fsym=$symbol&tsym=EUR&limit=$limit");
+	$graphdata = json_decode($graph, true);
+
+}
+$grapharray = array();
+$fields = array('Date,Open,High,Low,Close,Volume');
+fwrite($f, implode($fields, ',')."\n");
+foreach($graphdata['Data'] as $key => $value)
+{
+
+    if($_GET['type'] =='days'){
+        $ts = date("Y-m-d",$value['time']); 
     }
-    echo json_encode($grapharray);
+    if($_GET['type'] =='hours'){
+        $ts = date("Y-m-d H:i:s",$value['time']); 
+    }
+    if($_GET['type'] =='minutes'){
+        $ts = date("Y-m-d H:i:s",$value['time']); 
+    }
+    
+
+    $open = $value['open'];
+    $high = $value['high'];
+    $low = $value['low'];
+    $close = $value['close'];
+    $volume = $value['volumeto'];
+
+fputcsv($f, array("Date"=>$ts, "Open"=>$open, "High"=>$high, "Low"=>$low,"Close"=>$close, "Volume"=>$volume));
+}
+fclose($f);
 ?>
- 
